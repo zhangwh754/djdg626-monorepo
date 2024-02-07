4f880118e092a1c8c4ac25bf1e665dedb733bfb3
张文皓 <zhangwh754@163.com>
2024-02-07 17:27:33 +0800

    feat: 绑定data数据到vm实例上

--- packages/02-vue-mvvm/src/mvvm.ts
+++ packages/02-vue-mvvm/src/mvvm.ts
@@ -12,5 +12,26 @@ export default class Mvvm {
     this.$el = document.querySelector(options.el)!
     this.$options = options
     this.$data = options.data
+
+    this._proxyData(options.data)
+  }
+
+  _proxyData(data: Object) {
+    if (!data || typeof data !== 'object') {
+      throw new Error('data需要传入一个对象')
+    }
+
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        configurable: true,
+        enumerable: true,
+        get() {
+          return this.$data[key]
+        },
+        set(newValue) {
+          this.$data[key] = newValue
+        },
+      })
+    })
   }
 }
